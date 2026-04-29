170fe1df9363474267eefb50266d57ce1eec4771
Ranjith kannan U <ranjithkannanu@gmail.com>
2026-04-29 21:38:45 +0530

    Fix resume import path in Hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Hero.css'
 import Profile_img from '/src/assets/profile.JPG'
-import resume from '/src/assets/Ranjith kannan U Resume.pdf';
+import resume from '/src/assets/ranjith_kannan_U_resume.pdf';
 
 const Hero = () => {
   return (
